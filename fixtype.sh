302a2cd1c72e65a85e715c343fdc4f79ffdb6c52
Yuriy Shtokarev <shtokarev@gmail.com>
2023-03-16 21:12:27 +0300

    Update version to 0.0.2

--- fixtype.sh
+++ fixtype.sh
@@ -1,15 +1,13 @@
 #!/usr/bin/env bash
 
-chmod 0755 dist/cjs/src/bin.js
-
 cat >dist/cjs/package.json <<!EOF
 {
   "type": "commonjs"
 }
 !EOF
-
 cat >dist/mjs/package.json <<!EOF
 {
   "type": "module"
 }
 !EOF
+chmod 0755 dist/cjs/src/bin.js
